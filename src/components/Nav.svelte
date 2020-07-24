040960be5d455fb4ce6dc513c9b6e041fafe3dc7
saiphp <besabellacyrus@gmail.com>
2020-07-24 12:00:05 +0800

    added mobile navigation

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,75 +1,199 @@
 <script>
-  export let segment;
+  // export let segment;
+  let isMobileMenu = false;
+
+  function toggleMenu() {
+    isMobileMenu = !isMobileMenu;
+  }
 </script>
 
-<style>
-  /* nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
-
-	ul {
-		margin: 0;
-		padding: 0;
-	}
-
-	/* clearfix */
-  /* ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
-
-	li {
-		display: block;
-		float: left;
-	}
-
-	[aria-current] {
-		position: relative;
-		display: inline-block;
-	}
-
-	[aria-current]::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
-
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	}  */
+<style lang="scss">
+  .menu-wrapper {
+    display: flex;
+  }
+  .menu-desktop {
+    @media screen and (max-width: 696px) {
+      display: none;
+    }
+  }
+  .menu-icon {
+    display: none;
+    @media screen and (max-width: 696px) {
+      display: block;
+      height: 100%;
+      width: 3.5rem;
+      svg {
+        fill: #25577d;
+      }
+    }
+  }
+  .mobile-open {
+    left: 0 !important;
+  }
+  .mobile-menu {
+    height: 100%;
+    width: 100%;
+    transition: left 300ms ease;
+    left: -500px;
+    top: 0;
+    background-color: #fff;
+    position: absolute;
+    z-index: 9;
+    padding: 2rem;
+    display: flex;
+    .close-btn {
+      position: absolute;
+      right: 17px;
+      width: 2rem;
+      svg {
+        fill: #25577d;
+      }
+    }
+    & ul {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      & li {
+        display: flex;
+        justify-content: flex-start;
+      }
+    }
+    .mobile-menu-items {
+      display: flex;
+      flex-direction: column;
+      place-content: center;
+    }
+  }
 </style>
 
 <nav>
-  <ul>
-    <li>
-      <a aria-current={segment === undefined ? 'page' : undefined} href=".">
-        home
-      </a>
-    </li>
-    <li>
-      <a aria-current={segment === 'about' ? 'page' : undefined} href="about">
-        about
-      </a>
-    </li>
+  <div class="logo-wrapper">
+    <img src="/pgn_logo.svg" alt="" />
+  </div>
+  <div class="menu-desktop menu-wrapper">
+    <ul>
+      <li>Home</li>
+      <li>Episodes</li>
+      <li>About</li>
+      <li>Resources</li>
+      <li>Contact</li>
+    </ul>
+    <button class="listen-btn">
+      <span>Listen</span>
+      <span>
+        <svg
+          id="rss"
+          xmlns="http://www.w3.org/2000/svg"
+          width="17.5"
+          height="17.5"
+          viewBox="0 0 17.5 17.5">
+          <g id="Group_54" data-name="Group 54" transform="translate(0 12.672)">
+            <g id="Group_53" data-name="Group 53" transform="translate(0 0)">
+              <circle
+                id="Ellipse_8"
+                data-name="Ellipse 8"
+                cx="2.5"
+                cy="2.5"
+                r="2.5"
+                transform="translate(0 -0.173)"
+                fill="#fff" />
+            </g>
+          </g>
+          <g id="Group_56" data-name="Group 56" transform="translate(0 5.834)">
+            <g id="Group_55" data-name="Group 55">
+              <path
+                id="Path_386"
+                data-name="Path 386"
+                d="M0,149.344v3.334a8.342,8.342,0,0,1,8.334,8.333h3.332A11.68,11.68,0,0,0,0,149.344Z"
+                transform="translate(0 -149.344)"
+                fill="#fff" />
+            </g>
+          </g>
+          <g id="Group_58" data-name="Group 58">
+            <g id="Group_57" data-name="Group 57">
+              <path
+                id="Path_387"
+                data-name="Path 387"
+                d="M0,0V3.334A14.182,14.182,0,0,1,14.167,17.5H17.5A17.519,17.519,0,0,0,0,0Z"
+                fill="#fff" />
+            </g>
+          </g>
+        </svg>
+      </span>
+    </button>
+  </div>
+  <div class="menu-icon" on:click={toggleMenu}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+      <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+    </svg>
+  </div>
+  <div class="mobile-menu {isMobileMenu ? 'mobile-open' : ''}">
+    <div class="close-btn" on:click={toggleMenu}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+        <path
+          d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414
+          1.414L10 11.414l7.071 7.071 1.414-1.414L11.414
+          10l7.071-7.071-1.414-1.414L10 8.586z" />
+      </svg>
+    </div>
+    <div class="mobile-menu-items">
+      <ul>
+        <li>Home</li>
+        <li>Episodes</li>
+        <li>About</li>
+        <li>Resources</li>
+        <li>Contact</li>
+      </ul>
 
-    <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-    <li>
-      <a
-        rel="prefetch"
-        aria-current={segment === 'blog' ? 'page' : undefined}
-        href="blog">
-        blog
-      </a>
-    </li>
-  </ul>
+      <button class="listen-btn">
+        <span>Listen</span>
+        <span>
+          <svg
+            id="rss"
+            xmlns="http://www.w3.org/2000/svg"
+            width="17.5"
+            height="17.5"
+            viewBox="0 0 17.5 17.5">
+            <g
+              id="Group_54"
+              data-name="Group 54"
+              transform="translate(0 12.672)">
+              <g id="Group_53" data-name="Group 53" transform="translate(0 0)">
+                <circle
+                  id="Ellipse_8"
+                  data-name="Ellipse 8"
+                  cx="2.5"
+                  cy="2.5"
+                  r="2.5"
+                  transform="translate(0 -0.173)"
+                  fill="#fff" />
+              </g>
+            </g>
+            <g
+              id="Group_56"
+              data-name="Group 56"
+              transform="translate(0 5.834)">
+              <g id="Group_55" data-name="Group 55">
+                <path
+                  id="Path_386"
+                  data-name="Path 386"
+                  d="M0,149.344v3.334a8.342,8.342,0,0,1,8.334,8.333h3.332A11.68,11.68,0,0,0,0,149.344Z"
+                  transform="translate(0 -149.344)"
+                  fill="#fff" />
+              </g>
+            </g>
+            <g id="Group_58" data-name="Group 58">
+              <g id="Group_57" data-name="Group 57">
+                <path
+                  id="Path_387"
+                  data-name="Path 387"
+                  d="M0,0V3.334A14.182,14.182,0,0,1,14.167,17.5H17.5A17.519,17.519,0,0,0,0,0Z"
+                  fill="#fff" />
+              </g>
+            </g>
+          </svg>
+        </span>
+      </button>
+    </div>
+  </div>
 </nav>
