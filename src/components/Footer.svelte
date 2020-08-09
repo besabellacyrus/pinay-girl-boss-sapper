ebe8bf15c671dc3c6f27d5494dfd98e35ac6da54
saiphp <besabellacyrus@gmail.com>
2020-08-09 20:10:38 +0800

    fixed issues footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,65 +2,76 @@
 
 </script>
 
-<style>
-
+<style lang="scss">
+  .footer-wrapper-main {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    @media screen and (max-width: 768px) {
+      display: flex;
+      grid-template-columns: 1fr 1fr;
+      max-width: 1400px;
+      flex-direction: column-reverse;
+    }
+  }
 </style>
 
 <footer>
-  <div class="left-footer">
-    <div class="left-footer-content">
-      <div>
-        <img src="/img/girl-boss-logo.svg" alt="" />
+  <div class="footer-wrapper-main">
+    <div class="left-footer">
+      <div class="left-footer-content">
+        <div>
+          <img src="/img/girl-boss-logo.svg" alt="" />
+        </div>
+        <div>
+          <h3>Contact Us</h3>
+          <p>
+            123 Unit, Street Name, Municipality Name, City Name, Philippines
+            Area Code
+          </p>
+          <span>sample@gmail.com</span>
+        </div>
       </div>
-      <div>
-        <h3>Contact Us</h3>
-        <p>
-          123 Unit, Street Name, Municipality Name, City Name, Philippines Area
-          Code
-        </p>
-        <span>sample@gmail.com</span>
+      <div class="footer-links">
+        <h3>Privacy Policy</h3>
+        <h3>Terms of Service</h3>
+      </div>
+      <div class="footer-copy-rights">
+        &copy; 2020 The Pinay Girl Boss. All rights reserved.
       </div>
     </div>
-    <div class="footer-links">
-      <h3>Privacy Policy</h3>
-      <h3>Terms of Service</h3>
-    </div>
-    <div class="footer-copy-rights">
-      &copy; 2020 The Pinay Girl Boss. All rights reserved.
-    </div>
-  </div>
-  <div class="right-footer">
-    <div>
+    <div class="right-footer">
       <div>
-        <h3>Let's Get Social!</h3>
-        <div class="social-media">
-          <div>
-            <img src="/img/footer/facebook.svg" alt="" />
-          </div>
-          <div>
-            <img src="/img/footer/instagram.svg" alt="" />
-          </div>
-          <div>
-            <img src="/img/footer/messenger.svg" alt="" />
-          </div>
-          <div>
-            <img src="/img/footer/Twitter.svg" alt="" />
+        <div>
+          <h3>Let's Get Social!</h3>
+          <div class="social-media">
+            <div>
+              <img src="/img/footer/facebook.svg" alt="" />
+            </div>
+            <div>
+              <img src="/img/footer/instagram.svg" alt="" />
+            </div>
+            <div>
+              <img src="/img/footer/messenger.svg" alt="" />
+            </div>
+            <div>
+              <img src="/img/footer/Twitter.svg" alt="" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div>
       <div>
-        <h3>Subscribe To The Podcast</h3>
-        <div class="social-media">
-          <div>
-            <img src="/img/footer/rss.svg" alt="" />
-          </div>
-          <div>
-            <img src="/img/footer/Spotify.svg" alt="" />
-          </div>
-          <div>
-            <img src="/img/footer/iTunes.svg" alt="" />
+        <div>
+          <h3>Subscribe To The Podcast</h3>
+          <div class="social-media">
+            <div>
+              <img src="/img/footer/rss.svg" alt="" />
+            </div>
+            <div>
+              <img src="/img/footer/Spotify.svg" alt="" />
+            </div>
+            <div>
+              <img src="/img/footer/iTunes.svg" alt="" />
+            </div>
           </div>
         </div>
       </div>
